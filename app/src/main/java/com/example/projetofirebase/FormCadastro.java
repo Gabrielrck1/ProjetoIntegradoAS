c5d55d60175fa8529f75620a0f3dc9aa3de3e086
ganso <gabriel.rck1@gmail.com>
2024-04-20 00:37:54 -0300

    Novo campo de confirmação de senha
    Introdução futuro botão de recuperação de senha

--- app/src/main/java/com/example/projetofirebase/FormCadastro.java
+++ app/src/main/java/com/example/projetofirebase/FormCadastro.java
@@ -6,18 +6,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
@@ -27,6 +20,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class FormCadastro extends AppCompatActivity {
     private EditText edit_nome, edit_email, edit_senha, edit_senha2;
@@ -48,29 +42,26 @@ public class FormCadastro extends AppCompatActivity {
 
         IniciarComponentes();
 
-        bt_cadastrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        bt_cadastrar.setOnClickListener(v -> {
 
-                String nome = edit_nome.getText().toString();
-                String email = edit_email.getText().toString();
-                String senha = edit_senha.getText().toString();
-                String senha2 = edit_senha2.getText().toString();
+            String nome = edit_nome.getText().toString();
+            String email = edit_email.getText().toString();
+            String senha = edit_senha.getText().toString();
+            String senha2 = edit_senha2.getText().toString();
 
-                if (nome.isEmpty() || email.isEmpty() || senha.isEmpty() || senha2.isEmpty()) {
-                    Snackbar snackbar = Snackbar.make(v, mensagens[0], Snackbar.LENGTH_SHORT);
+            if (nome.isEmpty() || email.isEmpty() || senha.isEmpty() || senha2.isEmpty()) {
+                Snackbar snackbar = Snackbar.make(v, mensagens[0], Snackbar.LENGTH_SHORT);
+                snackbar.setBackgroundTint(Color.WHITE);
+                snackbar.setTextColor(Color.BLACK);
+                snackbar.show();
+            } else {
+                if ( senha.equals(senha2)) {
+                    CadastrarUsuario(v);
+                } else{
+                    Snackbar snackbar = Snackbar.make(v, mensagens[2], Snackbar.LENGTH_SHORT);
                     snackbar.setBackgroundTint(Color.WHITE);
                     snackbar.setTextColor(Color.BLACK);
                     snackbar.show();
-                } else {
-                    if ( senha.equals(senha2)) {
-                        CadastrarUsuario(v);
-                    } else{
-                        Snackbar snackbar = Snackbar.make(v, mensagens[2], Snackbar.LENGTH_SHORT);
-                        snackbar.setBackgroundTint(Color.WHITE);
-                        snackbar.setTextColor(Color.BLACK);
-                        snackbar.show();
-                    }
                 }
             }
         });
@@ -80,40 +71,37 @@ public class FormCadastro extends AppCompatActivity {
         String email = edit_email.getText().toString();
         String senha = edit_senha.getText().toString();
 
-        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha).addOnCompleteListener(task -> {
 
-                if (task.isSuccessful()) {
+            if (task.isSuccessful()) {
 
-                    SalvarDadosUsuario();
-                    Snackbar snackbar = Snackbar.make(v, mensagens[1], Snackbar.LENGTH_SHORT);
-                    snackbar.setBackgroundTint(Color.WHITE);
-                    snackbar.setTextColor(Color.BLACK);
-                    snackbar.show();
-                } else {
-                    String erro;
-                    try {
-                        throw task.getException();
+                SalvarDadosUsuario();
+                Snackbar snackbar = Snackbar.make(v, mensagens[1], Snackbar.LENGTH_SHORT);
+                snackbar.setBackgroundTint(Color.WHITE);
+                snackbar.setTextColor(Color.BLACK);
+                snackbar.show();
+            } else {
+                String erro;
+                try {
+                    throw Objects.requireNonNull(task.getException());
 
-                    } catch (FirebaseAuthWeakPasswordException e) {
-                        erro = "Digite uma senha com no minimo 6 numeros";
-                    } catch (FirebaseAuthUserCollisionException e) {
-                        erro = "Esta conta ja foi cadastrada";
-                    } catch (FirebaseAuthInvalidCredentialsException e) {
-                        erro = "Email inválido";
-                    } catch (Exception e) {
-                        erro = "Erro ao cadastrar usuário";
-                    }
-
-                    Snackbar snackbar = Snackbar.make(v, erro, Snackbar.LENGTH_SHORT);
-                    snackbar.setBackgroundTint(Color.WHITE);
-                    snackbar.setTextColor(Color.BLACK);
-                    snackbar.show();
-
+                } catch (FirebaseAuthWeakPasswordException e) {
+                    erro = "Digite uma senha com no minimo 6 numeros";
+                } catch (FirebaseAuthUserCollisionException e) {
+                    erro = "Esta conta ja foi cadastrada";
+                } catch (FirebaseAuthInvalidCredentialsException e) {
+                    erro = "Email inválido";
+                } catch (Exception e) {
+                    erro = "Erro ao cadastrar usuário";
                 }
 
+                Snackbar snackbar = Snackbar.make(v, erro, Snackbar.LENGTH_SHORT);
+                snackbar.setBackgroundTint(Color.WHITE);
+                snackbar.setTextColor(Color.BLACK);
+                snackbar.show();
+
             }
+
         });
     }
     private void SalvarDadosUsuario(){
@@ -123,22 +111,11 @@ public class FormCadastro extends AppCompatActivity {
         Map<String,Object> usuarios = new HashMap<>();
         usuarios.put("nome", nome);
 
-        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        usuarioID = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
 
         DocumentReference documentReference = db.collection("Usuarios").document(usuarioID);
-        documentReference.set(usuarios).addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void unused) {
-                Log.d("db", "Sucesso ao salvar os dados");
-
-            }
-        })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                    Log.d("db_error","Erro ao salvar os dados" + e.toString());
-                    }
-                });
+        documentReference.set(usuarios).addOnSuccessListener(unused -> Log.d("db", "Sucesso ao salvar os dados"))
+                .addOnFailureListener(e -> Log.d("db_error","Erro ao salvar os dados"));
     }
 
     private void IniciarComponentes(){
