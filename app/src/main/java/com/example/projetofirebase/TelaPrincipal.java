b5b9264e1180403ba6bee36e8d48b4670727c885
ganso <gabriel.rck1@gmail.com>
2024-04-21 13:13:07 -0300

    Nome de usuario, Email de usuario, Botão de retorno, Botão de logoff

--- app/src/main/java/com/example/projetofirebase/TelaPrincipal.java
+++ app/src/main/java/com/example/projetofirebase/TelaPrincipal.java
@@ -1,24 +1,55 @@
 package com.example.projetofirebase;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class TelaPrincipal extends AppCompatActivity {
+
+    private TextView nomeUsuario,emailUsuario;
+    private Button bt_deslogar;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String usuarioID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tela_principal);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        IniciarComponentes();
+
+        bt_deslogar.setOnClickListener(v -> {
+
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(TelaPrincipal.this, FormLogin.class);
+            startActivity(intent);
+            finish();
         });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        String email = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DocumentReference documentReference = db.collection("Usuarios").document(usuarioID);
+        documentReference.addSnapshotListener((documentSnapshot, error) -> {
+            if (documentSnapshot != null){
+                nomeUsuario.setText(documentSnapshot.getString("nome"));
+                emailUsuario.setText(email);
+            }
+        });
+    }
+
+    private void IniciarComponentes(){
+            nomeUsuario = findViewById(R.id.textNomeUsuario);
+            emailUsuario = findViewById(R.id.textEmailUsuario);
+            bt_deslogar = findViewById(R.id.bt_deslogar);
+        }
 }
