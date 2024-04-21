0af43f7184249beda4bcfd1955c1402725592908
ganso <gabriel.rck1@gmail.com>
2024-04-21 20:07:30 -0300

    Redefinição de senha OK + Corpo do email

--- app/src/main/java/com/example/projetofirebase/FormSenha.java
+++ app/src/main/java/com/example/projetofirebase/FormSenha.java
@@ -1,16 +1,18 @@
 package com.example.projetofirebase;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class FormSenha extends AppCompatActivity {
-
-    EditText edit_email; // Altere String para EditText
+    String[] mensagens = {"Por favor, insira um email cadastrado", "Enviamos o email de recuperação!"};
+    private EditText edit_email2;
 
     private static final String TAG = "FormSenha"; // Defina sua tag de log
 
@@ -18,6 +20,9 @@ public class FormSenha extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recuperar_senha);
+
+        // Encontrar a EditText no layout XML e atribuir a edit_email2
+        edit_email2 = findViewById(R.id.edit_email2);
 
         // Configurar o botão 'btn_voltar' para voltar uma tela
         Button btnVoltar = findViewById(R.id.seta2);
@@ -30,19 +35,33 @@ public class FormSenha extends AppCompatActivity {
 
     private void enviarEmailRedefinicaoSenha() {
         FirebaseAuth auth = FirebaseAuth.getInstance();
-        String email = edit_email.getText().toString().trim();
+        String email = edit_email2.getText().toString().trim();
 
         // Verificar se o campo de email está vazio
         if (email.isEmpty()) {
-            Toast.makeText(FormSenha.this, "Por favor, insira seu email", Toast.LENGTH_LONG).show();
-            return; // Encerrar o método se o campo de email estiver vazio
-        }
+            Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), mensagens[0], Snackbar.LENGTH_SHORT);
+            snackbar.setBackgroundTint(Color.WHITE);
+            snackbar.setTextColor(Color.BLACK);
+            snackbar.show();
+        } else {
+            auth.sendPasswordResetEmail(email)
+                    .addOnSuccessListener(aVoid -> {
+                        Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), mensagens[1], Snackbar.LENGTH_SHORT);
+                        snackbar.setBackgroundTint(Color.WHITE);
+                        snackbar.setTextColor(Color.BLACK);
+                        snackbar.show();
 
-        auth.sendPasswordResetEmail(email)
-                .addOnSuccessListener(aVoid -> Toast.makeText(FormSenha.this, "Enviamos um email de recuperação!", Toast.LENGTH_LONG).show())
-                .addOnFailureListener(e -> {
-                    // Tratar a falha ao enviar o email de redefinição de senha
-                    Toast.makeText(FormSenha.this, "Falha ao enviar email de recuperação: " + e.getMessage(), Toast.LENGTH_LONG).show();
-                    Log.e(TAG, "Falha ao enviar email de recuperação", e);
-                });
-    }}
+                        // Adicionar um atraso de 2 segundos antes de retornar
+                        // Retornar após 2 segundos
+                        new Handler().postDelayed(this::finish, 2000);
+                    })
+                    .addOnFailureListener(e -> {
+                        Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), "Falha ao enviar email de recuperação: " + e.getMessage(), Snackbar.LENGTH_SHORT);
+                        snackbar.setBackgroundTint(Color.WHITE);
+                        snackbar.setTextColor(Color.BLACK);
+                        snackbar.show();
+                        Log.e(TAG, "Falha ao enviar email de recuperação", e);
+                    });
+        }
+    }
+}
